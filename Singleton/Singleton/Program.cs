041a785b51121576c108c721adbfb36395d9c216
Spencer <spencermaas@outlook.com>
2017-12-15 13:22:18 -0800

    added property
    created property to test encapsulation

--- Singleton/Singleton/Program.cs
+++ Singleton/Singleton/Program.cs
@@ -6,7 +6,31 @@ namespace Singleton
     {
         public static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Singleton s = Singleton.getInstance();
+            Console.Write(s.getNum);
         }
+    }
+
+    class Singleton
+    {
+        //private static Singleton inst;
+        private static Singleton obj;
+        public int getNum{
+            get{
+                return 1;
+            }
+        }
+
+        private Singleton(){ }
+
+        /* not thread safe */
+        public static Singleton getInstance(){
+            if (obj == null){ // two threads may enter here at the same time and
+                              // and return different objects
+                obj = new Singleton(); 
+            }
+            return obj; 
+        }
+
     }
 }
